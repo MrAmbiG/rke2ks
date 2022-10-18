f19a06254c35c47f56bb9c8356795fb29983be79
amd User <gajeambi@amd.com>
2022-10-18 13:04:27 -0400

    rke2 stand alone cluster setup

--- rke2ks.sh
+++ rke2ks.sh
@@ -2,6 +2,9 @@
 echo "'run this as a sudo or root user 'sudo bash rke2ks.sh'"
 echo 'getting the latest kubectl binary'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+mv kubectl /usr/bin/kubectl
+chmod 777 /usr/bin/kubectl
+chmod +x /usr/bin/kubectl
 echo 'got it'
 echo 'getting rke2 binary'
 curl -sfL https://get.rke2.io | sh -
@@ -9,7 +12,8 @@ systemctl enable rke2-server.service
 systemctl start rke2-server.service
 echo 'waiting for 11 seconds'
 sleep 10s
-echo "KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
-echo "PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+echo "export PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+chmod 777 /etc/rancher/rke2/rke2.yaml
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml PATH=$PATH:/var/lib/rancher/rke2/bin
 source ~/.bashrc
