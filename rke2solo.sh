bdbf7def04299638efb1ced1f46664fffaa16951
amd User <gajeambi@amd.com>
2022-10-18 11:29:39 -0400

    echo commands

--- rke2solo.sh
+++ rke2solo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo ls
+echo 'getting the latest kubectl binary'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+echo 'got it'
+echo 'getting rke2 binary'
 curl -sfL https://get.rke2.io | sh -
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
