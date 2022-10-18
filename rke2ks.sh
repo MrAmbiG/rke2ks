138badb883442ea5c9ebf6b68d79709282402da1
amd User <gajeambi@amd.com>
2022-10-18 13:30:14 -0400

    rke2ks, updated env vars

--- rke2ks.sh
+++ rke2ks.sh
@@ -3,8 +3,6 @@ echo "'run this as a sudo or root user 'sudo bash rke2ks.sh'"
 echo 'getting the latest kubectl binary'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 mv kubectl /usr/bin/kubectl
-chmod 777 /usr/bin/kubectl
-chmod +x /usr/bin/kubectl
 echo 'got it'
 echo 'getting rke2 binary'
 curl -sfL https://get.rke2.io | sh -
@@ -14,6 +12,9 @@ echo 'waiting for 11 seconds'
 sleep 10s
 echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
 echo "export PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/bin/kubectl" >> ~/.bashrc
 chmod 777 /etc/rancher/rke2/rke2.yaml
-export KUBECONFIG=/etc/rancher/rke2/rke2.yaml PATH=$PATH:/var/lib/rancher/rke2/bin
+chmod 777 /usr/bin/kubectl
+chmod +x /usr/bin/kubectl
+export KUBECONFIG=/etc/rancher/rke2/rke2.yaml PATH=$PATH:/var/lib/rancher/rke2/bin PATH=$PATH:/usr/bin/kubectl
 source ~/.bashrc
