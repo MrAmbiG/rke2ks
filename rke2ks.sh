33bf76fe708ba01322750f7d7cc15a67d918431b
amd User <gajeambi@amd.com>
2022-11-29 12:02:30 -0500

    moved up the config lines

--- rke2ks.sh
+++ rke2ks.sh
@@ -6,6 +6,8 @@ mv kubectl /usr/bin/kubectl
 echo 'got it'
 echo 'getting rke2 binary'
 curl -sfL https://get.rke2.io | sh -
+echo 'write-kubeconfig-mode: "0644"' >> /etc/rancher/rke2/rke2.yaml
+echo 'disable: rke2-ingress-nginx' >> /etc/rancher/rke2/rke2.yaml
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
 echo 'waiting for 11 seconds'
@@ -13,8 +15,6 @@ sleep 10s
 echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
 echo "export PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
 echo "export PATH=$PATH:/usr/bin/kubectl" >> ~/.bashrc
-echo 'write-kubeconfig-mode: "0644"' >> /etc/rancher/rke2/rke2.yaml
-echo 'disable: rke2-ingress-nginx' >> /etc/rancher/rke2/rke2.yaml
 chmod 777 /etc/rancher/rke2/rke2.yaml
 chmod 777 /usr/bin/kubectl
 chmod +x /usr/bin/kubectl
