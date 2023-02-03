a3dc73f840f37bdc0808427db0792946b7a4f89f
amd <gajeambi@amd.com>
2023-02-03 18:20:55 +0000

    worked node config

---
+++ rke2ks_add_node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copy this script to the node, replace MASTER1 witht he ip or domain name of the master1 (controller) of the k8s
+# replace MASTER_TOKEN with master token from /var/lib/rancher/rke2/server/token from master1
+sudo mkdir -p /etc/rancher/rke2/
+rm -rf config.yaml
+echo "server: https://MASTER1:9345" > config.yaml
+echo "token: MASTER_TOKEN" >> config.yaml
+sudo cp config.yaml /etc/rancher/rke2/
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" INSTALL_RKE2_VERSION=v1.21.14+rke2r1 sh -
+systemctl enable rke2-agent.service
+systemctl start rke2-agent.service
