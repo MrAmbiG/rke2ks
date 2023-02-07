b7f1ab6779777d6f8b62409741addb9ae7478b10
amd <gajeambi@amd.com>
2023-02-07 10:01:07 +0000

    add args

--- rke2ks_add_node.sh
+++ rke2ks_add_node.sh
@@ -5,6 +5,6 @@ sudo mkdir -p /etc/rancher/rke2/
 rm -rf /etc/rancher/rke2/config.yaml
 echo "server: https://MASTER1:9345" > /etc/rancher/rke2/config.yaml
 echo "token: MASTER_TOKEN" >> /etc/rancher/rke2/config.yaml
-curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" INSTALL_RKE2_VERSION=v1.21.14+rke2r1 sh -
-systemctl enable rke2-agent.service
-systemctl start rke2-agent.service
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" INSTALL_RKE2_VERSION=v1.21.14+rke2r1 sudo sh -
+sudo systemctl enable rke2-agent.service
+sudo systemctl start rke2-agent.service
